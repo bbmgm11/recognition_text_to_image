5c4170f2af7b80a8f06cba06da8ed2aed47ee28c
Александр <veshev38@bk.ru>
2024-01-14 08:02:59 +0800

    #8 доработал интерфейс, добавил возможность переводить расшифрованный текст

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 import easyocr
 import os
 import cv2
+import translators
+
 
 class Interface(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Распознавание текста")
         self.resize(750,700)
+
+        self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.center()
 
         #основной фрейм на котором все кнопки, лайбл и фрейм 3
         self.frame_4 = QtWidgets.QFrame(self)
-        self.frame_4.setGeometry(QtCore.QRect(0, 0, 750, 700))
+        self.frame_4.setGeometry(QtCore.QRect(0, 0, 728, 700))
         self.frame_4.setStyleSheet("QFrame{\n"
                                    "    color: white;\n"
                                    "    border-radius: 7px;\n"
@@ -86,17 +90,23 @@ class Interface(QtWidgets.QWidget):
         self.pushButton_2.setDefault(False)
         self.pushButton_2.clicked.connect(lambda: self.showMinimized())
 
+        #дроп и драг
+       # self.listvig = QtWidgets.QListWidget(self)
+       # self.listvig.setGeometry(QtCore.QRect(-150, 50, 1100,1100))
+       # self.listvig.setDragDropMode(QtWidgets.QAbstractItemView.DragDrop)
+
+
         #лайбл
         self.image_label = QtWidgets.QLabel(self.frame_4)
-        self.image_label.resize(751, 581)
-        self.image_label.setGeometry(QtCore.QRect(0, 30, 741, 556))
-        self.image_label.setTextFormat(QtCore.Qt.AutoText)
+        self.image_label.setGeometry(QtCore.QRect(0, 30, 729, 556))
+        image = QtGui.QPixmap("icon/download.png")
+        self.image_label.setPixmap(image)
         self.image_label.setScaledContents(True)
         self.image_label.setWordWrap(False)
 
         #кнопка выбрать изображение
         self.load_button = QtWidgets.QPushButton("Выбрать изображение", self.frame_4)
-        self.load_button.setGeometry(QtCore.QRect(250, 588, 261, 30))
+        self.load_button.setGeometry(QtCore.QRect(10, 610, 261, 30))
         font = QtGui.QFont()
         font.setPointSize(11)
         font.setBold(True)
@@ -121,7 +131,7 @@ class Interface(QtWidgets.QWidget):
 
         # кнопка распознования
         self.decode_button = QtWidgets.QPushButton("Распознать", self.frame_4)
-        self.decode_button.setGeometry(QtCore.QRect(250, 623, 261, 30))
+        self.decode_button.setGeometry(QtCore.QRect(10, 650, 261, 30))
         font = QtGui.QFont()
         font.setPointSize(11)
         font.setBold(True)
@@ -146,11 +156,71 @@ class Interface(QtWidgets.QWidget):
         layout = QtWidgets.QVBoxLayout(self)
         layout.addWidget(self.frame_4)
 
+        # кнопка перевода
+        self.decode_translate = QtWidgets.QPushButton("Перевести", self.frame_4)
+        self.decode_translate.setGeometry(QtCore.QRect(450, 665, 271, 20))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.decode_translate.setFont(font)
+        self.decode_translate.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.decode_translate.setStyleSheet("QPushButton{\n"
+                                       "    color: white;\n"
+                                       "    border-radius: 7px;\n"
+                                       "    background-color: #6495ed;\n"
+                                       "}\n"
+                                       "\n"
+                                       "QPushButton:hover{\n"
+                                       "    background-color: #50566E;\n"
+                                       "}\n"
+                                       "\n"
+                                       "QPushButton:pressed{\n"
+                                       "    background-color: #434965;\n"
+                                       "}")
+        self.decode_translate.clicked.connect(self.translator)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.frame_4)
+
+        #кнопка ru и en и лейб выбора языка
+        self.lang_sel = QtWidgets.QLabel("Язык текста:", self.frame_4)
+        self.lang_sel.setGeometry(QtCore.QRect(450, 620, 271, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.lang_sel.setFont(font)
+
+        self.ru_button = QtWidgets.QRadioButton("Русский", self.frame_4)
+        self.ru_button.setGeometry(QtCore.QRect(590, 640, 131, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.ru_button.setFont(font)
+        self.ru_button.setStyleSheet("QRadioButton{\n"
+                                         "    color: white;\n"
+                                         "    border-radius: 7px;\n"
+                                         "}")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("icon/Russia.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.ru_button.setIcon(icon1)
+        self.ru_button.setChecked(True)
+
+        self.en_button = QtWidgets.QRadioButton("Английский", self.frame_4)
+        self.en_button.setGeometry(QtCore.QRect(450, 640, 131, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.en_button.setFont(font)
+        self.en_button.setStyleSheet("QRadioButton{\n"
+                                         "    color: white;\n"
+                                         "    border-radius: 7px;\n"
+                                         "}")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icon/UnitedKingdom.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.en_button.setIcon(icon)
 
 
     def image_loade(self):
         file_dealog = QtWidgets.QFileDialog()
-        file_puth, _ = file_dealog.getOpenFileName(self, "Выбирите изображение")
+        file_puth, _ = file_dealog.getOpenFileName(self, "Выбeрите изображение")
 
         if file_puth:
             pixmap = QtGui.QPixmap(file_puth)
@@ -190,6 +260,52 @@ class Interface(QtWidgets.QWidget):
             self.oldPos = event.globalPos()
         except AttributeError:
             pass
+
+
+    def dropEvent(self, event):
+        print('drop')
+    def dragMoveEvent(self, event):
+        print('move')
+    def dragEnterEvent(self, event):
+        print("вв")
+
+
+    def translator(self, ru_button):
+        pixmap = self.image_label.pixmap()
+        if pixmap:
+            image = pixmap.toImage()
+            image.save("temp.png")
+            img = cv2.imread("temp.png")
+            reader = easyocr.Reader(["ru", "en"])
+            resault = reader.readtext(img, detail=0, paragraph=True)
+
+
+            with open("result.txt", "w") as file:
+                for line in resault:
+                    file.write(f"{line}\n\n")
+            file_patch = os.path.join("result.txt")
+            with open(file_patch, "r") as file:
+                file_content = file.read()
+
+            if self.ru_button.isChecked() == True:
+                self.trans_resault = translators.translate_text(query_text=file_content, translator="google", from_language="ru", to_language="en")
+
+                with open("translation.txt", "w") as file:
+                    for line in self.trans_resault:
+                        file.write(f"{line}")
+
+            elif self.en_button.isChecked() == True:
+                self.trans_resault = translators.translate_text(query_text=file_content, translator="google", from_language="en", to_language="ru")
+
+                with open("translation.txt", "w") as file:
+                    for line in self.trans_resault:
+                        file.write(f"{line}")
+
+            os.remove("result.txt")
+            os.startfile("translation.txt")
+            os.remove("temp.png")
+
+
 
 if __name__ == "__main__":
     import sys
